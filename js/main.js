d31252aad5d6e7347ff863fd41d0a0a6669b59b6
madhurip22 <67985826+madhurip22@users.noreply.github.com>
2025-10-28 00:57:23 -0700

    Add toggleMenu function for navigation display

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 
+function toggleMenu() {
+    const nav = document.querySelector('.nav-links');
+    if (nav.style.display === 'flex') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'flex';
+    }
+}
